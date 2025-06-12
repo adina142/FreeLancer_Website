bd1278655b39253d7cd5e7355df622c6c34f4dc1
WardahKhurram127 <149463720+WardahKhurram127@users.noreply.github.com>
2025-06-12 17:48:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/jobs', require('./routes/jobRoutes'));
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
