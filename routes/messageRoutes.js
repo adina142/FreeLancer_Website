ae0fb5f67990ef98bb50917ee421bf49d339aa44
yumna617 <yumna.shahid617@gmail.com>
2025-06-15 19:50:45 +0500

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/auth');
 const { sendMessage } = require('../controllers/advancedFeatureController');
-
-// ✅ Define the route
 router.post('/send', authenticate, sendMessage);
 
 module.exports = router;
